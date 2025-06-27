8d3d8b3dfb8c6720a0f61553200f74598f707508
Linus Claußen <105798691+Linus-Cl@users.noreply.github.com>
2025-06-27 19:53:20 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # ==== Google Doc Download url ==== #
 url = "https://drive.google.com/uc?export=download&id=1dMJ5nniCYicuOyf6JYXhNymbn7yikGuV"
 
-# ==== List of all Players ==== # (need to adjust dcc.Dropdown in app.py (line 85) for more or less players)
+# ==== List of all Players ==== # 
 players = ["Bobo", "Phil", "Steven"]
